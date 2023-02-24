c2828d743f1cdbca1370e786ab64e62279075e6d
ianrich69420 <ian.orieko@student.moringaschool.com>
2023-02-24 15:06:13 +0300

    Completed assignment

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,13 @@
-class Student 
+require_relative './user'
 
+class Student < User
+    attr_reader :knowledge
+
+    def initialize
+        @knowledge = []
+    end
+
+    def learn string
+        self.knowledge << string
+    end
 end
